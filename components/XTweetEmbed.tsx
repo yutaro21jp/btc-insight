3a038f76e6d8d397da15839f60b92dcb4d5304c8
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2025-12-29 06:01:08 +0700

    Twitter Embed内の動画が再生されない不具合を改善しました（その６）。

--- components/XTweetEmbed.tsx
+++ components/XTweetEmbed.tsx
@@ -1,57 +1,25 @@
-'use client'
-
-import { useEffect, useMemo, useRef } from 'react'
+import Script from 'next/script'
 
 type XTweetEmbedProps = {
   url: string
 }
 
-declare global {
-  interface Window {
-    twttr?: {
-      widgets: {
-        load: (el?: HTMLElement) => void
-      }
-    }
-  }
-}
-
 export default function XTweetEmbed({ url }: XTweetEmbedProps) {
-  const containerRef = useRef<HTMLDivElement>(null)
-  const canonicalUrl = useMemo(() => {
+  const canonicalUrl = (() => {
     const statusMatch = url.match(/status\/(\d+)/)
     if (statusMatch?.[1]) {
       return `https://twitter.com/i/web/status/${statusMatch[1]}`
     }
     return url
-  }, [url])
-
-  useEffect(() => {
-    const existingScript = document.querySelector<HTMLScriptElement>(
-      'script[data-x-widgets]'
-    )
-
-    const load = () => {
-      if (window.twttr?.widgets?.load) {
-        window.twttr.widgets.load(containerRef.current ?? undefined)
-      }
-    }
-
-    if (existingScript) {
-      load()
-      return
-    }
-
-    const script = document.createElement('script')
-    script.src = 'https://platform.twitter.com/widgets.js'
-    script.async = true
-    script.setAttribute('data-x-widgets', 'true')
-    script.onload = load
-    document.body.appendChild(script)
-  }, [canonicalUrl])
+  })()
 
   return (
-    <div ref={containerRef}>
+    <div>
+      <Script
+        id="x-widgets"
+        src="https://platform.twitter.com/widgets.js"
+        strategy="afterInteractive"
+      />
       <blockquote className="twitter-tweet">
         <a href={canonicalUrl}>View on X</a>
       </blockquote>
