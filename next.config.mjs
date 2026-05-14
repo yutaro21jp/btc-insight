ab8853b7e0363b7497d1a7d0ae8201d53f3e43f6
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2026-05-14 22:29:27 +0700

    Reduce Next image transformations

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    formats: ['image/webp'],
+    minimumCacheTTL: 2678400,
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
